98831d23fe0936c56a7c497c611adc77529405e2
Dominik <dominikhallmann9@gmail.com>
2021-09-26 08:57:37 +0200

    cwiczenie1

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,14 @@ public class Main {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
         System.out.println("Witaj świecie!");
+        System.out.println("To jest nasz pierwszy program w języku java");
+        System.out.println("Życzę" + " " + "wam " + "miłej nauki!");
+
+        System.out.printf("A %s zaczynamy z %d przykładem.\n", "więc", 1);
+
+        System.out.print("To jest jakiś");
+        System.out.print(" tekst");
+        System.out.print(" nie za długi.");
 
 
     }
