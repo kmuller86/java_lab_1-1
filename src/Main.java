8367e44609a04a2b35cf78d90b606088f0c20015
Dominik <dominikhallmann9@gmail.com>
2021-09-26 09:21:16 +0200

    cwiczenie2

--- src/Main.java
+++ src/Main.java
@@ -11,13 +11,28 @@ public class Main {
         /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
 
         /*
-        *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
-        *    mnożenia, dziealenia i modulo na zmiennych:
-        *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
-        *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
-        *  */
-
-
-
+         *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
+         *    mnożenia, dziealenia i modulo na zmiennych:
+         *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
+         *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
+         *  */
+        byte zmien1 = 2;
+        short zmien2 = 23;
+        int zmien3 = 3333;
+        long zmien4 = 33333333;
+        float zmien5 = 33.33f;
+        double zmien6 = 1212.1212;
+        boolean zmienbol = true;
+        char zmienchar = 'w';
+        String hehe = "ehehe";
+        System.out.println("zmienna zmien1 = " + zmien1);
+        System.out.println("zmienna zmien2 = " + zmien2);
+        System.out.println("zmienna zmien3 = " + zmien3);
+        System.out.println("zmienna zmien4 = " + zmien4);
+        System.out.println("zmienna zmien5 = " + zmien5);
+        System.out.println("zmienna zmien6 = " + zmien6);
+        System.out.println("zmienna boolean = " + zmienbol);
+        System.out.println("zmienna char = " + zmienchar);
+        System.out.println("string hehe = " + hehe);
     }
 }
