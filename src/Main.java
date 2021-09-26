6843799fbfed76ab835f93cbf3908052b5f76aa6
Dominik <dominikhallmann9@gmail.com>
2021-09-26 08:53:11 +0200

    komentarz

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
+        System.out.println("Witaj świecie!");
 
 
     }
