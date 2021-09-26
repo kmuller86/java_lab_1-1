d94b4372f5da63eca99f8d54da0df803203c004b
Dominik <dominikhallmann9@gmail.com>
2021-09-26 10:24:06 +0200

    cwiczenie3

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,25 @@ public class Main {
         /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
         *   wynik wyświetlić w konsoli programu
         */
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Podaj imie:");
+//        String imie = scan.next();
+//        System.out.print("Podaj nazwisko:");
+//        String nazwisko = scan.next();
+//        System.out.print("Podaj wiek:");
+//        int wiek = scan.nextInt();
+//        System.out.print("Podaj numer indeksu:");
+//        int nrindex = scan.nextInt();
+//        System.out.println("Imie: " + imie);
+//        System.out.println("Nazwisko: " + nazwisko);
+//        System.out.printf("Twój wiek to %d i numer indeksu to %d \n", + wiek, nrindex);
 
+        int A = 2333;
+        int B = 34;
+        System.out.println("wynik dodawania zmiennej a i b wynosi: " + (A+B));
+        System.out.println("wynik odejmowania zmiennej a i b wynosi: " + (A-B));
+        System.out.println("wynik mnożenia zmiennej a i b wynosi: " + (A*B));
+        System.out.println("wynik dzielenia zmiennej a i b wynosi: " + (A/B));
+        System.out.println("wynik dzielenia z resztą zmiennej a i b wynosi: "+ (A%B));
     }
 }
