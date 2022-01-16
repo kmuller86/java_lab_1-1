e8165c704374e711c47d5666107c424df028a296
kmuller86 <89707079+kmuller86@users.noreply.github.com>
2022-01-16 23:36:06 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,19 @@ public class Main {
         przypisać do niej wartości i wyświetlić */
         int[] tablica = new int[5];
         for (int i = 0; i < 5; i++)
+        // po co typuje pan i na int skoro on jest intem?
             tablica [(int) i] = i +1;
         for (int i = 0; i < 5; i++)
             System.out.println("Następna komórka to: " + tablica[i]);
+        
+        /*
+        pan stworzył tablicę int a nie double
+        powinno byc:
+        double[] tabDouble = new double[5];
+        for(int i=0; i<tabDouble.length; i++) {
+        tabDouble[i] = i + 0.2 // wartosc dowolna
+        }
+        
+        */
     }
 }
